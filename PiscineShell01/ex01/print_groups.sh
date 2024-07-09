7a696ebe0ab08615b8d6c1e9385be056e116f322
Raquel Larielis <rqlprodigy@gmail.com>
2024-07-09 16:06:06 +0200

    ex01

--- PiscineShell01/ex01/print_groups.sh
+++ PiscineShell01/ex01/print_groups.sh
@@ -2,4 +2,4 @@
 # Obtener la lista de grupos del usuario
 # G : grupos al que pertenece el usuario (muestra valores numericos)
 # n : convierte los numeros de los grupos a nombres
-echo "$(id -Gn $FT_USER)" | tr " " ","
+id -Gn $FT_USER | tr ' ' ',' | tr -d "\n"
