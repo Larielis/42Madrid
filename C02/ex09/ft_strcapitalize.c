747f94f3e5258ad1977401378e97988502a44e5e
Larielis <racamach@student.42madrid.com>
2024-07-10 20:47:36 +0200

    ex09

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 19:25:27 by racamach          #+#    #+#             */
-/*   Updated: 2024/07/10 12:48:49 by racamach         ###   ########.fr       */
+/*   Updated: 2024/07/10 20:46:52 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char	*ft_strcapitalize(char *str);
 int		ft_is_alpha(char c);
 void	ft_to_lowercase(char *c);
 void	ft_to_uppercase(char *c);
+char	*ft_strlowercase(char *str);
 
 char	*ft_strcapitalize(char *str)
 {
@@ -24,20 +25,19 @@ char	*ft_strcapitalize(char *str)
 
 	i = 0;
 	new_word = 1;
+	ft_strlowercase(str);
 	while (str[i] != '\0')
 	{
 		if (ft_is_alpha(str[i]))
 		{
 			if (new_word)
-			{
 				ft_to_uppercase(&str[i]);
-				new_word = 0;
-			}
+			new_word = 0;
 		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			new_word = 0;
 		else
-		{
 			new_word = 1;
-		}
 		i++;
 	}
 	return (str);
@@ -64,10 +64,23 @@ void	ft_to_uppercase(char *c)
 	}
 }
 
+char	*ft_strlowercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_to_lowercase(&str[i]);
+		i++;
+	}
+	return (str);
+}
+
 /* int	main(void)
 {
-	char test1[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
-	printf("%s \n", ft_strcapitalize(test1));
+	char	test1[] = "salut,comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	printf("\n %s \n", ft_strcapitalize(test1));
 	return (0);
 }
  */
