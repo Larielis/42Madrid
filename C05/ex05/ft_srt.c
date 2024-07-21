c9989d8644aed4771ce82efab255ccca6cf6e875
Raquel Larielis <rqlprodigy@gmail.com>
2024-07-21 17:54:36 +0200

    ex05

--- C05/ex05/ft_srt.c
+++ C05/ex05/ft_srt.c
@@ -6,21 +6,39 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 21:06:23 by racamach          #+#    #+#             */
-/*   Updated: 2024/07/20 21:41:31 by racamach         ###   ########.fr       */
+/*   Updated: 2024/07/21 16:12:10 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	ft_sqrt(int nb)
 {
-	int	temp;
-	int	result;
+	int	i;
 
-	result = nb / 2;
-	temp = 0;
-	while (temp != nb)
+	i = 0;
+	if (nb < 0)
 	{
-		temp = result;
-		result = (nb / temp + temp) / 2;
+		return (0);
 	}
-	return (result);
+	while (i * i <= nb)
+	{
+		if (i * i == nb)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	printf("%d \n", ft_sqrt(-4));
+	printf("%d \n", ft_sqrt(0));
+	printf("%d \n", ft_sqrt(1));
+	printf("%d \n", ft_sqrt(4));
+	printf("%d \n", ft_sqrt(9));
+	printf("%d \n", ft_sqrt(16));
+	printf("%d \n", ft_sqrt(25));
 }
