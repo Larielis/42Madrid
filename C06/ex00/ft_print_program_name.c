9c90346855aec35a0e74ea315c02eaa2169f678d
Raquel Larielis <rqlprodigy@gmail.com>
2024-07-21 20:59:55 +0200

    ex00

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 17:55:59 by racamach          #+#    #+#             */
+/*   Updated: 2024/07/21 18:03:05 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (!argc)
+	{
+		return (1);
+	}
+	ft_putstr(argv[0]);
+	return (0);
+}
