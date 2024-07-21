c8a958b041ae0e194df48be9dd3f34ab212ad5ad
Raquel Larielis <rqlprodigy@gmail.com>
2024-07-21 21:00:03 +0200

    ex01

---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 18:03:40 by racamach          #+#    #+#             */
+/*   Updated: 2024/07/21 18:10:05 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (!argc)
+	{
+		return (1);
+	}
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		i++;
+	}
+	return (0);
+}
