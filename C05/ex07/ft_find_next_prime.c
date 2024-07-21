673af6f0d4b2f6f1a8eca40601718f5c563c5b66
Raquel Larielis <rqlprodigy@gmail.com>
2024-07-21 17:54:49 +0200

    ex07

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 16:45:46 by racamach          #+#    #+#             */
+/*   Updated: 2024/07/21 17:22:08 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+	{
+		return (0);
+	}
+	i = 2;
+	while (i * i <= nb)
+	{
+		if (nb % i == 0)
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	while (!ft_is_prime(nb))
+	{
+		nb++;
+	}
+	return (nb);
+}
+
+int	main(void)
+{
+	printf("%d \n", ft_find_next_prime(6));
+}
