08260d91e5bccee0ac80729cee6db60c9e32e856
Raquel Larielis <rqlprodigy@gmail.com>
2024-07-21 17:54:30 +0200

    ex04

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -6,9 +6,11 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 20:32:50 by racamach          #+#    #+#             */
-/*   Updated: 2024/07/20 20:57:41 by racamach         ###   ########.fr       */
+/*   Updated: 2024/07/21 15:34:04 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	ft_fibonacci(int index)
 {
@@ -19,4 +21,14 @@ int	ft_fibonacci(int index)
 	if (index == 1)
 		return (1);
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
+int	main(void)
+{
+	printf("%d ", ft_fibonacci(0));
+	printf("%d ", ft_fibonacci(1));
+	printf("%d ", ft_fibonacci(2));
+	printf("%d ", ft_fibonacci(3));
+	printf("%d ", ft_fibonacci(4));
+	printf("%d ", ft_fibonacci(5));
 }
