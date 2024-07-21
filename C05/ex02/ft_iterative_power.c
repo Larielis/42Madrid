84bc5dc60ef5ece5ff234e6ffe4261a6d59f5e65
Raquel Larielis <rqlprodigy@gmail.com>
2024-07-21 17:54:07 +0200

    ex02

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -6,9 +6,11 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 16:27:12 by racamach          #+#    #+#             */
-/*   Updated: 2024/07/18 18:35:55 by racamach         ###   ########.fr       */
+/*   Updated: 2024/07/21 15:23:38 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	ft_iterative_power(int nb, int power);
 
@@ -16,7 +18,7 @@ int	ft_iterative_power(int nb, int power)
 {
 	int	result;
 
-	result = 0;
+	result = 1;
 	if (power < 0)
 		return (0);
 	else if (nb == 0 && power == 0)
@@ -26,4 +28,17 @@ int	ft_iterative_power(int nb, int power)
 		result = result * nb;
 		power--;
 	}
+	return (result);
+}
+
+int main(void)
+{	
+	printf("%d \n", ft_iterative_power(0, 0));
+	printf("%d \n", ft_iterative_power(0, 1));
+	printf("%d \n", ft_iterative_power(1, 0));
+	printf("%d \n", ft_iterative_power(3, 2));
+	printf("%d \n", ft_iterative_power(3, -2));
+	printf("%d \n", ft_iterative_power(-3, 3));
+	printf("%d \n", ft_iterative_power(-3, 2));
+	return (0);
 }
