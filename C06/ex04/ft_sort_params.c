8d0f702d9a2be8a6219b309c3796d7ca4cbb3cc7
Raquel Larielis <rqlprodigy@gmail.com>
2024-07-21 21:00:16 +0200

    ex04

---
+++ C06/ex04/ft_sort_params.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 19:50:07 by racamach          #+#    #+#             */
+/*   Updated: 2024/07/21 20:24:25 by racamach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int		ft_strcmp(char *s1, char *s2);
+void	ft_swap(char **a, char **b);
+void	ft_sort_int_tab(int argc, char **argv);
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		}
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_int_tab(int argc, char **argv)
+{
+	int	swapped;
+	int	i;
+	int	j;
+
+	i = 0;
+	swapped = 1;
+	while (i < argc - 1 && swapped)
+	{
+		j = 1;
+		swapped = 0;
+		while (j < argc - i - 1)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				ft_swap(&argv[j], &argv[j + 1]);
+				swapped = 1;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc <= 1)
+	{
+		return (0);
+	}
+	ft_sort_int_tab(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		i++;
+	}
+	return (0);
+}
