bf07643b11c318f4c5e4c8fb356f8cf84c88f59e
Raquel Larielis <rqlprodigy@gmail.com>
2024-07-21 17:54:00 +0200

    ex01

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -6,14 +6,28 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 13:35:40 by racamach          #+#    #+#             */
-/*   Updated: 2024/07/18 13:39:26 by racamach         ###   ########.fr       */
+/*   Updated: 2024/07/21 17:49:11 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	ft_recursive_factorial(int nb)
 {
-	if (nb <= 1)
+	if (nb < 0)
+	{
+		return (0);
+	}
+	if (nb == 1 || nb == 0)
 		return (1);
 	else
-		return (nb * factorial(nb - 1));
+		return (nb * ft_recursive_factorial(nb - 1));
+}
+
+int	main(void)
+{
+	printf("%d \n", ft_recursive_factorial(-1223));
+	printf("%d \n", ft_recursive_factorial(0));
+	printf("%d \n", ft_recursive_factorial(1));
+	printf("%d \n", ft_recursive_factorial(2));
 }
